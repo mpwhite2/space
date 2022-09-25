c416737aafde0f76a34036e568e64be532f4cd09
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-09-24 19:44:58 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c b . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 2 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . e 2 . . . . . . . 
+. . . . . . e e 4 e . . . . . . 
+. . . . . . e 2 4 e . . . . . . 
+. . . . . c c c e e e . . . . . 
+. . . . e e 2 2 2 4 e e . . . . 
+. . c f f f c c e e f f e e . . 
+. c c c c e e 2 2 2 2 4 2 e e . 
+c c c c c c e e 2 2 2 4 2 2 e e 
+c c c c c c e e 2 2 2 2 4 2 e e 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,80 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.pewPew.play()
+    Laser = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . 7 . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Projectile)
+    Laser.x = Ship.x
+    Laser.y = Ship.y
+    Laser.setVelocity(0, -100)
+    if (Laser.x == 100) {
+        Laser.destroy()
+    }
+})
+let Bomb: Sprite = null
+let Laser: Sprite = null
+let Ship: Sprite = null
+Ship = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . . 2 2 2 . . . . . . . 
+    . . . . . 2 f f f 2 . . . . . . 
+    . . . . . 2 2 2 2 2 . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Ship.setStayInScreen(true)
+controller.moveSprite(Ship)
+let Shoots = 4
+forever(function () {
+    if (controller.B.isPressed() && Shoots > 0) {
+        music.thump.play()
+        Bomb = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . d d d . . . . . . . 
+            . . . . . d d d d d . . . . . . 
+            . . . . . . 4 4 4 . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Player)
+        Bomb.x = Ship.x
+        Bomb.y = Ship.y
+        Bomb.setVelocity(0, -69)
+        pause(200)
+        Bomb.destroy(effects.fire, 500)
+        Shoots += -1
+    }
+})
